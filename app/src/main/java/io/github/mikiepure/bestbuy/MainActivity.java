f9722dea0490af12643c45b8b86bc6ae26ea1e3c
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2020-06-19 23:11:00 +0900

    Support spotbugs on gradle script

--- app/src/main/java/io/github/mikiepure/bestbuy/MainActivity.java
+++ app/src/main/java/io/github/mikiepure/bestbuy/MainActivity.java
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public final class MainActivity extends AppCompatActivity {
 
-  protected Calculator calc1 = null;
-  protected Calculator calc2 = null;
+  Calculator calc1 = null;
+  Calculator calc2 = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -14,22 +14,24 @@ public final class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    this.calc1 =
-        new Calculator(
-            findViewById(R.id.textInputEditTextPrice1),
-            findViewById(R.id.textInputEditTextVolume1),
-            findViewById(R.id.textInputEditTextNumber1),
-            findViewById(R.id.textInputEditTextUnitPrice1),
-            findViewById(R.id.buttonClear1),
-            getString(R.string.invalidTextMessage));
-    this.calc2 =
-        new Calculator(
-            findViewById(R.id.textInputEditTextPrice2),
-            findViewById(R.id.textInputEditTextVolume2),
-            findViewById(R.id.textInputEditTextNumber2),
-            findViewById(R.id.textInputEditTextUnitPrice2),
-            findViewById(R.id.buttonClear2),
-            getString(R.string.invalidTextMessage));
+    if (this.calc1 == null) {
+      this.calc1 = new Calculator(
+          findViewById(R.id.textInputEditTextPrice1),
+          findViewById(R.id.textInputEditTextVolume1),
+          findViewById(R.id.textInputEditTextNumber1),
+          findViewById(R.id.textInputEditTextUnitPrice1),
+          findViewById(R.id.buttonClear1),
+          getString(R.string.invalidTextMessage));
+    }
+    if (this.calc2 == null) {
+      this.calc2 = new Calculator(
+          findViewById(R.id.textInputEditTextPrice2),
+          findViewById(R.id.textInputEditTextVolume2),
+          findViewById(R.id.textInputEditTextNumber2),
+          findViewById(R.id.textInputEditTextUnitPrice2),
+          findViewById(R.id.buttonClear2),
+          getString(R.string.invalidTextMessage));
+    }
   }
 
   @Override
