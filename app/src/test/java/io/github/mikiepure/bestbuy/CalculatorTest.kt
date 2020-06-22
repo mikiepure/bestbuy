7b06015d3ccd77042399d552b6df07ff14ea4dc9
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2020-06-22 23:44:02 +0900

    Add jacoco for unit test coverage report

--- app/src/test/java/io/github/mikiepure/bestbuy/CalculatorTest.kt
+++ app/src/test/java/io/github/mikiepure/bestbuy/CalculatorTest.kt
@@ -1,71 +1,85 @@
 package io.github.mikiepure.bestbuy
 
 import android.content.Context
+import android.widget.Button
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.textfield.TextInputEditText
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.math.BigDecimal
 
 @RunWith(AndroidJUnit4::class)
 class CalculatorTest {
+
+    private lateinit var textInputEditTextPrice: TextInputEditText
+    private lateinit var textInputEditTextVolume: TextInputEditText
+    private lateinit var textInputEditTextNumber: TextInputEditText
+    private lateinit var textInputEditTextUnitPrice: TextInputEditText
+    private lateinit var buttonClear: Button
+    private lateinit var calculator: Calculator
+
+    @Before
+    fun setUp() {
+        val c = ApplicationProvider.getApplicationContext<Context>()
+        this.textInputEditTextPrice = TextInputEditText(c)
+        this.textInputEditTextVolume = TextInputEditText(c)
+        this.textInputEditTextNumber = TextInputEditText(c)
+        this.textInputEditTextUnitPrice = TextInputEditText(c)
+        this.buttonClear = Button(c)
+        this.calculator = Calculator(textInputEditTextPrice, textInputEditTextVolume, textInputEditTextNumber, textInputEditTextUnitPrice, buttonClear, "error message")
+    }
+
     @Test
     fun beforeTextChanged() {
-        val c = ApplicationProvider.getApplicationContext<Context>()
-        val price = TextInputEditText(c)
-        val volume = TextInputEditText(c)
-        val number = TextInputEditText(c)
-        val unitPrice = TextInputEditText(c)
-        Calculator(price, volume, number, unitPrice).beforeTextChanged("", 0, 0, 0)
+        this.calculator.beforeTextChanged("", 0, 0, 0)
     }
 
     @Test
     fun onTextChanged() {
-        val c = ApplicationProvider.getApplicationContext<Context>()
-        val price = TextInputEditText(c)
-        val volume = TextInputEditText(c)
-        val number = TextInputEditText(c)
-        val unitPrice = TextInputEditText(c)
-        Calculator(price, volume, number, unitPrice).onTextChanged("", 0, 0, 0)
+        this.calculator.onTextChanged("", 0, 0, 0)
     }
 
     @Test
     fun afterTextChanged() {
-        val c = ApplicationProvider.getApplicationContext<Context>()
-        val price = TextInputEditText(c)
-        val volume = TextInputEditText(c)
-        val number = TextInputEditText(c)
-        val unitPrice = TextInputEditText(c)
-        Calculator(price, volume, number, unitPrice).afterTextChanged(price.text)
+        this.calculator.afterTextChanged(this.textInputEditTextUnitPrice.text)
     }
 
     @Test
-    fun getUnitPriceStr() {
+    fun onClick() {
+        this.calculator.onClick(null);
+    }
+
+    @Test
+    fun makeUnitPriceStr() {
         val method = Calculator::class.java.getDeclaredMethod(
-            "getUnitPriceStr",
-            String::class.java, String::class.java, String::class.java
+                "makeUnitPriceStr",
+                String::class.java, String::class.java, String::class.java
         )
         method.isAccessible = true
 
-        Assert.assertEquals(method.invoke(null, "100", "1", "1"), "100.00")
-        Assert.assertEquals(method.invoke(null, "100", "", ""), "100.00")
-        Assert.assertEquals(method.invoke(null, "100", "2", "1"), "50.00")
-        Assert.assertEquals(method.invoke(null, "100", "2", ""), "50.00")
-        Assert.assertEquals(method.invoke(null, "100", "1", "2"), "50.00")
-        Assert.assertEquals(method.invoke(null, "100", "", "2"), "50.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "1", "1"), "100.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "", ""), "100.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "2", "1"), "50.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "2", ""), "50.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "1", "2"), "50.00")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "", "2"), "50.00")
 
-        Assert.assertEquals(method.invoke(null, "", "", ""), "")
-        Assert.assertEquals(method.invoke(null, "100", "0", "1"), "")
-        Assert.assertEquals(method.invoke(null, "100", "1", "0"), "")
+        Assert.assertEquals(method.invoke(this.calculator, "", "", ""), "")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "0", "1"), "")
+        Assert.assertEquals(method.invoke(this.calculator, "100", "1", "0"), "")
+
+        /* NumberFormatException will be occurred at each string */
+        Assert.assertEquals(method.invoke(this.calculator, ".", ".", "."), "")
     }
 
     @Test
     fun calcUnitPrice() {
         val method = Calculator::class.java.getDeclaredMethod(
-            "calcUnitPrice",
-            BigDecimal::class.java, BigDecimal::class.java, BigDecimal::class.java
+                "calcUnitPrice",
+                BigDecimal::class.java, BigDecimal::class.java, BigDecimal::class.java
         )
         method.isAccessible = true
 
