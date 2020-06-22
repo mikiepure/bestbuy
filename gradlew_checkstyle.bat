4cd04dfedb134fc30e55d1f2f4131f8c8296c85f
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2020-06-22 23:06:38 +0900

    Support SpotBugs latest version

--- gradlew_checkstyle.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-
-set JAVA_HOME="C:\Program Files\Android\Android Studio\jre"
-gradlew checkstyle
---
+++ gradlew_linter.ps1
@@ -0,0 +1,23 @@
+$env:JAVA_HOME = "C:\Program Files\Android\Android Studio\jre"
+
+# CheckStyle
+.\gradlew.bat checkstyle
+if ($LastExitCode -ne 0) {
+    Write-Error "CheckStyle was failed by error code: $LastExitCode"
+    Write-Host -NoNewLine 'Press any key to exit...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    exit 1
+}
+Start-Process "build\reports\checkstyle\checkstyle.html"
+
+# SpotBugs
+.\gradlew.bat spotbugsMain
+if ($LastExitCode -ne 0) {
+    Write-Error "SpotBugs was failed by error code: $LastExitCode"
+    Write-Host -NoNewLine 'Press any key to exit...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    exit 1
+}
+Start-Process "app\build\reports\spotbugs\main.html"
+
+exit 0
