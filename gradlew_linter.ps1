eb724e58e521b65fd3d15b1849b33cd0a02386eb
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2020-06-23 01:37:21 +0900

    Fix command line tools

--- gradlew_linter.ps1
+++ gradlew_linter.ps1
@@ -1,11 +1,12 @@
 $env:JAVA_HOME = "C:\Program Files\Android\Android Studio\jre"
+$env:ANDROID_SDK_ROOT = "C:\Users\Hiromitsu\AppData\Local\Android\Sdk"
 
 # CheckStyle
 .\gradlew.bat checkstyle
 if ($LastExitCode -ne 0) {
     Write-Error "CheckStyle was failed by error code: $LastExitCode"
-    Write-Host -NoNewLine 'Press any key to exit...';
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    Write-Host -NoNewLine "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit 1
 }
 Start-Process "build\reports\checkstyle\checkstyle.html"
@@ -14,8 +15,8 @@ Start-Process "build\reports\checkstyle\checkstyle.html"
 .\gradlew.bat spotbugsMain
 if ($LastExitCode -ne 0) {
     Write-Error "SpotBugs was failed by error code: $LastExitCode"
-    Write-Host -NoNewLine 'Press any key to exit...';
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    Write-Host -NoNewLine "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit 1
 }
 Start-Process "app\build\reports\spotbugs\main.html"
---
+++ gradlew_unittest.ps1
@@ -0,0 +1,14 @@
+$env:JAVA_HOME = "C:\Program Files\Android\Android Studio\jre"
+$env:ANDROID_SDK_ROOT = "C:\Users\Hiromitsu\AppData\Local\Android\Sdk"
+
+# Jacoco
+.\gradlew.bat test jacocoTestReport
+if ($LastExitCode -ne 0) {
+    Write-Error "Jacoco was failed by error code: $LastExitCode"
+    Write-Host -NoNewLine "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit 1
+}
+Start-Process "app\build\reports\jacoco\jacocoTestReport\html\index.html"
+
+exit 0
